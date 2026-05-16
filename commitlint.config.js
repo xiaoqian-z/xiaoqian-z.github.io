7e8b7c30a64744e9878346856908048b4b157699
xiaoqina-z <3269674365@qq.com>
2026-05-16 23:05:53 +0800

    feat: second blog

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+  },
 }
